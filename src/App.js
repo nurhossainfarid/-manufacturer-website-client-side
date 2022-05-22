d370ddb169b205a6026ac19b2dc47c18f363b1e2
nurhossainfarid <faahsan1516@gmail.com>
2022-05-22 19:53:43 +0600

    install daisyapu query and set initial setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h3>Welcome To .carBro</h3>
+      <button class="btn btn-ghost">Button</button>
     </div>
   );
 }
